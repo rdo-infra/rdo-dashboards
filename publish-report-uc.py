6579a66806641a7b7b7856d7c43e5e44334744a5
Joel Capitao <jcapitao@redhat.com>
2020-09-04 16:21:42 +0200

    Search in CentOS base repos for report-uc dashboard
    Change-Id: I57f1ab19a13dcde808a1e9ccb792e9b452accdc0

--- publish-report-uc.py
+++ publish-report-uc.py
@@ -14,8 +14,13 @@ report = importlib.import_module('report-uc')
 repos_url = ['https://trunk.rdoproject.org/centos8-master/current/delorean.repo',
              'https://trunk.rdoproject.org/centos8-master/delorean-deps.repo']
 
+repos = ['BaseOS,http://mirror.regionone.rdo-cloud.rdoproject.org/centos/8/BaseOS/x86_64/os/',
+         'AppStream,http://mirror.regionone.rdo-cloud.rdoproject.org/centos/8/AppStream/x86_64/os/',
+         'extras,http://mirror.regionone.rdo-cloud.rdoproject.org/centos/8/extras/x86_64/os/',
+         'PowerTools,http://mirror.regionone.rdo-cloud.rdoproject.org/centos/8/PowerTools/x86_64/os/']
+
 rows = []
-for uc in report.provides_uc('master', 'centos8', repos_url, [], None,
+for uc in report.provides_uc('master', 'centos8', repos_url, repos, None,
                              '', 'cbs'):
     if uc.pkg_name == '':
         continue
